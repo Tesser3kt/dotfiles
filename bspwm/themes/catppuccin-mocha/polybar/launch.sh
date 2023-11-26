0b46bb156a7990a7e5d8fbd39095856d062d138b
Tesser3kt <djklepy@gmail.com>
2023-11-26 16:35:05 +0100

    added bspwm config

---
+++ bspwm/themes/catppuccin-mocha/polybar/launch.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CARD="$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)"
+INTERFACE="$(ip link | awk '/state UP/ {print $2}' | tr -d :)"
+RFILE="$DIR/.module"
+INTERNAL_MONITOR=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1)
+EXTERNAL_MONITOR=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | tail -n 1)
+
+# Fix backlight and network modules
+fix_modules() {
+	if [[ -z "$CARD" ]]; then
+		sed -i -e 's/backlight/bna/g' "$DIR"/config.ini
+	elif [[ "$CARD" != *"intel_"* ]]; then
+		sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
+	fi
+
+	if [[ "$INTERFACE" == e* ]]; then
+		sed -i -e 's/network/ethernet/g' "$DIR"/config.ini
+	fi
+}
+
+# Launch the bar
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	MONITOR=INTERNAL_MONITOR polybar -q main -c "$DIR"/config.ini &
+	MONITOR=EXTERNAL_MONITOR polybar -q external -c "$DIR"/config.ini &
+	# Launch the bar
+	# for mon in $(polybar --list-monitors | cut -d":" -f1); do
+	# 	MONITOR=$mon polybar -q main -c "$DIR"/config.ini &
+	# done
+}
+
+# Execute functions
+if [[ ! -f "$RFILE" ]]; then
+	fix_modules
+	touch "$RFILE"
+fi	
+launch_bar
