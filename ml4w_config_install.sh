bbca08fb69e954e071001f31ce53e46df4fc8590
Adam Klepac <djklepy@gmail.com>
2024-09-17 17:05:20 +0200

    added automated install script

---
+++ ml4w_config_install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "This script will backup and overwrite your current Alacritty, zshrc and Hyprland configs."
+echo -n "Proceed? y/n: "
+read -r proceed
+
+if [ $proceed = "y" ]; then
+    for folder in "alacritty" "zshrc" "hypr"
+    do
+        # Make backups
+        mv $HOME/.config/${folder} $HOME/.config/${folder}.bak
+        # Create symbolic links
+        ln -s $PWD/${folder} $HOME/.config/${folder}
+    done
+    echo "Your kernel files were successfully deleted."
+fi
+exit 0
